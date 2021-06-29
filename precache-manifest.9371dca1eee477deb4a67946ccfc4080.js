3ac5d901b746399e55d67c18a45e5e4248b5477d
robertbaum <robert.baum@itm-development.com>
2021-06-29 14:44:27 +0300

    Updates

---
+++ precache-manifest.9371dca1eee477deb4a67946ccfc4080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09ba5325fd11271d12c6657303fc86f",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "401729afb78502be1fca",
+    "url": "/monsters-react/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "5a5b5d4d61784f131205",
+    "url": "/monsters-react/static/js/2.89420f1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.89420f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401729afb78502be1fca",
+    "url": "/monsters-react/static/js/main.649f3610.chunk.js"
+  },
+  {
+    "revision": "18626d956b167bc951bc",
+    "url": "/monsters-react/static/js/runtime-main.6455e694.js"
+  }
+]);
